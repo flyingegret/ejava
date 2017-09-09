0a0088443321d1ee0a6450ed6caa97075d268952
flyingegret <906932256@qq.com>
2017-09-09 16:23:15 +0800

    first Hellojava file

---
+++ src/com/flying/egret/HelloJava.java
@@ -0,0 +1,10 @@
+package com.flying.egret;
+
+/**
+ * Created by egret on 2017/9/9.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.println("hi, this is egret!");
+    }
+}
