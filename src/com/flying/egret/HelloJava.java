13f89243a03d9a2647f2ccdf7f957d51cd77155c
flyingegret <906932256@qq.com>
2017-09-09 16:36:47 +0800

    modify HelloJava

--- src/com/flying/egret/HelloJava.java
+++ src/com/flying/egret/HelloJava.java
@@ -1,10 +1,14 @@
 package com.flying.egret;
 
+import java.util.Date;
+
 /**
  * Created by egret on 2017/9/9.
  */
 public class HelloJava {
     public static void main(String[] args){
         System.out.println("hi, this is egret!");
+        System.out.println(new Date());
+
     }
 }
